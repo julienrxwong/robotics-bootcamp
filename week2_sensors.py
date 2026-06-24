25faee6e64eec036ffc1ca6fd9ced96e39c33dc5
Julien <julienrxwong@gmail.com>
2026-06-23 19:23:00 -0700

    Week 2: sensor transforms starter file

---
+++ week2_sensors.py
@@ -0,0 +1,17 @@
+# Week 2 - Sensor Transforms (Starter Template)
+
+print("Robot Bootcamp - Week 2")
+
+# Example sensor values (fake data for now)
+distance_sensor = 120  # cm
+temperature_sensor = 22.5  # Celsius
+
+# Simple processing example
+if distance_sensor < 50:
+    print("WARNING: Object too close!")
+else:
+    print("Path is clear")
+
+# Print sensor readings
+print("Distance:", distance_sensor)
+print("Temperature:", temperature_sensor)
